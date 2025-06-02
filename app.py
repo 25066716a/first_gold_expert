390fe881d05d54757287c844b82f4716f2361ded
25066716a <kaikaicheng0409@gmail.com>
2025-06-02 17:15:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def calculate_score(answers, job, region_answer):
         elif '寶雅' in base_name:
             score += 0.5 if region_answer == '是' else 0.2
 
-    if job_name.strip() == '瓦城 外場服務員':
+    if job_name.strip() == '瓦城(外場服務員)':
     try:
         if float(answers[13]) >= 40:
             score += 1.0
